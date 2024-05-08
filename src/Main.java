bfe93b6b145d0e3892b8c526ddf96c5fed9e50dc
User <elmir81java@gmail.com>
2024-05-08 17:53:32 +0600

    hjgh

---
+++ src/Main.java
@@ -0,0 +1,119 @@
+import java.nio.channels.SocketChannel;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+////
+////        Scanner scanner =new Scanner(System.in);
+////        String elmir =scanner.nextLine();
+////        switch (elmir){
+////            case "usd":
+////                System.out.println("usd 89.29");
+////                break;
+////            case "eur":
+////                System.out.println("eur =97.07");
+////                break;
+////            case "rub":
+////                System.out.println("1.01");
+////                break;
+////            case "lira":
+////                System.out.println("lira=2.96");
+////                break;
+////
+////        }
+////
+////        Scanner scanner = new Scanner(System.in);
+////        int a = scanner.nextInt();
+////        System.out.println(a > 18 ? "Armiyaga barsa bolot" : "Armiyaga bargany jashy jetpeit");
+//// jashy 18 chon bolso Armiyaga barsa bolot dep chygyshy kerek
+//// jashy 18 kichuu bolso Armiyaga bargany jashy jetpeit dep chygyshy kerek
+//// jashy 18 barabar bolso Jashy barabar dep chygyshy kerek
+//
+//
+////        Scanner scanner1 = new Scanner(System.in);
+////        int ekzamen = scanner1.nextInt();
+////        System.out.println(ekzamen > 50 ? "ekzamenden ottunuz" : "ekzamendenkaldynyz
+//
+//
+//         Scanner scan=new Scanner(System.in);
+//
+//          int jahy=scan.nextInt();
+//          String rezolt=(jahy>18)?"jogoru":(jahy<18)?"kihy":"=";
+//        System.out.println(rezolt);
+//
+//         Scanner scan=new Scanner(System.in);
+//        System.out.println("Enter time :");
+//         int number= scan.nextInt();
+//         if (number>=12&&number<19){
+//             System.out.println("Good afternoom!");
+//
+//
+//
+//
+//                }
+
+
+//
+//        Scanner scanner=new Scanner(System.in);
+//        int ottu =scanner.nextInt();
+//        String kaldy=ottu>=50?"ottunuz":"kaldynyz";
+//
+//        System.out.println(kaldy);
+//
+//          Scanner scanner=new Scanner(System.in);
+//          int birsan=scanner.nextInt();
+//          while (birsan > 1 ) {
+//              System.out.println(birsan);
+//              birsan--;
+////          }
+//
+////
+////        Scanner scanner=new Scanner(System.in);
+////        int ekisan=scanner.nextInt();
+////        int birsan=scanner.nextInt();
+////        while (ekisan<=birsan){
+////            System.out.println(ekisan);
+////            ekisan++;}
+//
+//           Scanner scanner=new Scanner(System.in);
+//           int birsan=scanner.nextInt();
+//           while (birsan>1){
+////               System.out.println(birsan);birsan--;}
+//
+//
+//            Scanner scanner=new Scanner(System.in);
+//            int ekisan =scanner.nextInt();
+//            int birsan=scanner.nextInt();
+//            while (ekisan<=birsan){
+//                System.out.println(ekisan);
+//                ekisan++;
+//            }
+//
+//
+//             Scanner scanner=new Scanner(System.in);
+//             System.out.println("sanberiniz:");
+//             int number=scanner.nextInt();
+//             for (int i=0;i<number;i++){
+//            System.out.print(" * ");}
+//
+//
+//        Scanner scanner=Scanner(System.in);
+//        int nunber=scanner.nextInt();
+//        int a1=0;
+//        for (int i=1;i<nunber;i++){//i=1,
+//            nunber=10;i++=2;;
+//            if (i%2==0){//i=4
+//                System.out.println(i+"");a1+=i;}
+
+//
+//       Scanner el=new Scanner(System.in);
+//       int num1={35,53,56,56,76,};
+
+
+
+
+
+    }
+
